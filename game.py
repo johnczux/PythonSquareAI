7db4d1899f2383ebc5dd4fe91aac9c5e91219a3d
Jonathan <johnczux@gmail.com>
2021-05-11 14:54:14 -0400

    Added goal lines

--- game.py
+++ game.py
@@ -30,13 +30,24 @@ outsideBarrier = [[[50,50],[1000,50]],[[1000,50],[1300,200]],[[1300,200],[1380,3
 
 insideBarrier = [[[200,200],[1000,200]],[[1000,200],[1200,300]],[[1200,300],[1200,475]],[[1200,475],[1100,550]],[[1100,550],[200,625]],[[200,625],[200,200]] ]
 
-for outsideWall in outsideBarrier :
-     pygame.draw.line(overlay, (255,255,255), outsideWall[0] , outsideWall[1], 3)
-for insideWall in insideBarrier :
-     pygame.draw.line(overlay, (255,255,255), insideWall[0] , insideWall[1], 3)
+goalLines = [[[200,200],[200,50]],[[400,200],[400,50]],[[600,200],[600,50]],[[800,200],[800,50]],[[1000,200],[1000,50]],[[1200,300],[1200,150]],[[1200,400],[1385,400]]
+,[[1100,550],[1360,550]],[[900,570],[900,710]],[[700,585],[700,720]],[[500,600],[500,730]],[[300,620],[300,740]],[[200,625],[50,750]],[[200,550],[50,550]],[[200,350],[50,350]] ]
+
+def drawGoalLines():
+    for line in goalLines :
+        pygame.draw.line(overlay, (0,255,0), line[0] , line[1], 3)
+    pygame.draw.line(overlay, (255,150,0), (200,200) , (50,200))
 
 
-pygame.display.update()
+def drawTrack():
+    for outsideWall in outsideBarrier :
+        pygame.draw.line(overlay, (255,255,255), outsideWall[0] , outsideWall[1], 3)
+    for insideWall in insideBarrier :
+        pygame.draw.line(overlay, (255,255,255), insideWall[0] , insideWall[1], 3)
+
+
+drawGoalLines()
+drawTrack()
 #16ms = about 60 fps ( to get formula is 1 / fps amount * 1000)
 pygame.time.set_timer(1, 16)
 
@@ -48,12 +59,16 @@ def reset() :
     global Player_Y 
     global InputX 
     global InputY
+    global goalLines
 
+    goalLines = [[[200,200],[200,50]],[[400,200],[400,50]],[[600,200],[600,50]],[[800,200],[800,50]],[[1000,200],[1000,50]],[[1200,300],[1200,150]],[[1200,400],[1385,400]]
+,[[1100,550],[1360,550]],[[900,570],[900,710]],[[700,585],[700,720]],[[500,600],[500,730]],[[300,620],[300,740]],[[200,625],[50,750]],[[200,550],[50,550]],[[200,350],[50,350]] ]
+    
     InputX = 0
     InputY = 0
     Player_X = 100
     Player_Y = 100
-
+    drawGoalLines()
 
 def update():
     clock.tick()
@@ -91,6 +106,13 @@ def update():
     screen.blit(overlay, (0,0))
     player = pygame.draw.rect(screen, Player_Color, (int(Player_X), int(Player_Y), Player_Size, Player_Size))
 
+    for line in goalLines :
+        if player.clipline(line[0], line[1]):
+            del goalLines[0]
+            overlay.fill((0,0,0))
+            drawGoalLines()
+            drawTrack()
+
     for outsideWall in outsideBarrier :
         if player.clipline(outsideWall[0], outsideWall[1]):
             reset()
@@ -98,6 +120,7 @@ def update():
     for insideWall in insideBarrier :
         if player.clipline(insideWall[0], insideWall[1]):
             reset()
+
 
 
 
